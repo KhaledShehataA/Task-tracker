73be1349777f072ad0b73597b03d02efcf2a4758
KhaledShehataA <khaled.ah.shehata@gmail.com>
2021-04-20 07:09:58 +0200

    feat: Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,17 @@
-import styles from'./styles.css';
 import PropTypes from 'prop-types'
+import {Button} from '../Button'
+import styles from'./styles.css'
 
 
 export const Header = ({title}) => {
+    const onClick = () => {
+        console.log('Click')
+    }
+
     return (
         <header className="header">
             <h1>{title}</h1>
-            <button className="btn">Add</button>
+            <Button color='green' text='Add' onClick= {onClick}/>
         </header>
     )
 }
