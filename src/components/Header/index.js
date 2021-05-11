b8af5fe68d7b00dc542bf4f46037612fc8c97207
KhaledShehataA <khaled.ah.shehata@gmail.com>
2021-05-11 06:27:37 +0200

    feat: Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,15 +3,13 @@ import {Button} from '../Button'
 import styles from'./styles.css'
 
 
-export const Header = ({title}) => {
-    const onClick = () => {
-        console.log('Click')
-    }
+export const Header = ({title, onAdd, showAdd}) => {
+   
 
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick= {onClick}/>
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick= {onAdd}/>
         </header>
     )
 }
