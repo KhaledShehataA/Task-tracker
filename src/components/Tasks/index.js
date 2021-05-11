87bdcc808298d16af6d00019ed08035000dcce72
KhaledShehataA <khaled.ah.shehata@gmail.com>
2021-05-11 06:28:17 +0200

    feat: updated Tasks component

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -1,11 +1,16 @@
 import styles from './styles.css';
 import { Task } from '../Task/index';
 
-export const Tasks = ({ tasks }) => {
+export const Tasks = ({ tasks, onDelete, onToggle }) => {
 	return (
 		<>
 			{tasks.map((task) => (
-				<Task key={task.id} task={task} />
+				<Task
+					key={task.id}
+					task={task}
+					onDelete={onDelete}
+					onToggle={onToggle}
+				/>
 			))}
 		</>
 	);
